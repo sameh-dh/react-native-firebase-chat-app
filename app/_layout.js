f5fb3bc282490644594494aa9125a58c0dbaf69b
sameh-dh <samehdh44@gmail.com>
2024-09-14 11:38:25 +0100

    layout authentication rounter in process

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,35 @@
-import {Slot} from "expo-router";
+import React, { useEffect } from "react";
+import { Slot, useSegments, useRouter } from "expo-router";
+import { AuthContextProvider, useAuth } from "../context/authcontext";
 
 // Import your global CSS file
-import "../global.css"
+import "../global.css";
+const MainLayout = () => {
+  const { isAuthenticated } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
 
-export default Slot
+  useEffect(() => {
+    // check if the user is authenticated
+    if(typeof isAuthenticated == "undefined") return;
+    const inApp = segments[0] == '(app)';
+    if(isAuthenticated && !inApp){
+      // redirect user to home
+      router.replace('home');
+    }else if(isAuthenticated== false){
+        // redirect user to signIn
+        router.replace('signIn');
+
+    }
+  }, [isAuthenticated]);  
+
+  return <Slot />;
+};
+
+export default RootLayout = () => {
+  return (
+    <AuthContextProvider>
+      <MainLayout />
+    </AuthContextProvider>
+  );
+};
